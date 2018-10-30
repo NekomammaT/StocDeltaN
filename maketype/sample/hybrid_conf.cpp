3ce3766d28210ecc9ea3a43e81c905d5602fc17c
Yuichiro Tada <yuichiro.t.tada@gmail.com>
2018-10-30 18:44:30 +0900

    0.3.3

--- maketype/sample/hybrid_conf.cpp
+++ maketype/sample/hybrid_conf.cpp
@@ -1,36 +1,45 @@
 #include "../source/StocDeltaN_conf.hpp"
 #include <sys/time.h>
 
-#define MODEL "hilltop_conf" // model name
+#define MODEL "hybrid_conf" // model name
 
 // ---------- box size & step h ------------
-#define XMIN -21
-#define XMAX 21
-#define HMIN (1e-8)
-#define HOV (1./20) // h/|phi|
+#define PHIMIN 0.1409
+#define PHIMAX 0.142
+#define PSIMIN -(1e-3)
+#define PSIMAX (1e-3)
+#define HPHI (1e-5)
+#define HPSIOPSI (1e-2) // hpsi/|psi|
+#define HPSIMIN (1e-10)
 // -----------------------------------------
 
 // ---------- for PDE ----------------------
-#define MAXSTEP 100000000 // max recursion
+#define MAXSTEP 100000 // max recursion
 #define TOL 1e-10 // tolerance
 // -----------------------------------------
 
 // ---------- potential parameter ----------
-#define LAMBDA 0.01
-#define MU 20
+#define AS (2.189e-9)
+#define PI2 50
+#define MM 0.1
+#define PHIC (0.1*sqrt(2))
+#define MU2 11
+#define MU1 (PI2/MM/MM/PHIC)
+#define LAMBDA4 (AS*12*M_PI*M_PI/MU1/MU1)
 // -----------------------------------------
 
-#define RHOC (LAMBDA*LAMBDA*LAMBDA*LAMBDA*(sqrt(1+2*MU*MU)-1)/MU/MU) // end of inflation
+#define RHOC (2.074038e-16) // end of inflation
 
 // ---------- for SDE ----------------------
-#define RECURSION 100 // recursion for power spectrum
-#define XIN (1e-6) // i.c. for phi
-#define TIMESTEP (1e-1) // time step: delta N
+#define RECURSION 10000 // recursion for power spectrum
+#define PHIIN 0.1418 // i.c. for phi
+#define PSIIN 0 // i.c. for psi
+#define TIMESTEP (1e-3) // time step: delta N
 // -----------------------------------------
 
 // ---------- for power spectrum -----------
-#define DELTAN 10 // calc. PS every DELTAN e-folds
-#define NMAX 2800 // calc. PS for 0--NMAX e-folds
+#define DELTAN 0.1 // calc. PS every DELTAN e-folds
+#define NMAX 28 // calc. PS for 0--NMAX e-folds
 // -----------------------------------------
 
 
@@ -42,15 +51,23 @@ int main(int argc, char** argv)
   double before, after;
   
   gettimeofday(&tv, &tz);
-  before = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6;
+  before = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6; 
   // -----------------------------------------
 
   // ---------- set box step h ---------------
-  double h, sitev = XMIN;
+  double h = HPHI, sitev = PHIMIN;
   vector<double> site;
   vector< vector<double> > sitepack;
-  while (sitev <= XMAX) {
-    h = max(fabs(sitev)*HOV,HMIN);
+  while (sitev <= PHIMAX) {
+    site.push_back(sitev);
+    sitev += h;
+  }
+  sitepack.push_back(site);
+  site.clear();
+
+  sitev = PSIMIN;
+  while (sitev <= PSIMAX) {
+    h = max(fabs(sitev)*HPSIOPSI,HPSIMIN);
 
     site.push_back(sitev);
     sitev += h;
@@ -58,8 +75,8 @@ int main(int argc, char** argv)
   sitepack.push_back(site);
   site.clear();
   // ------------------------------------------
-
-  vector<double> xi = {XIN}; // set i.c. for sample paths
+  
+  vector<double> xi = {PHIIN,PSIIN}; // set i.c. for sample paths
   
   StocDeltaN sdn(MODEL,sitepack,RHOC,xi,0,MAXSTEP,TOL,RECURSION,
 		 TIMESTEP,NMAX,DELTAN); // declare the system
@@ -68,8 +85,8 @@ int main(int argc, char** argv)
   //sdn.sample_logplot(); // plot that sample path
   
   sdn.solve(); // solve PDE & SDE and obtain power spectrum
-  sdn.f1_loglinearplot(); // show plot of <N>
-  sdn.g2_loglinearplot(); // show plot of <delta N^2>
+  sdn.f1_logplot(); // show plot of <N>
+  sdn.g2_logplot(); // show plot of <delta N^2>
   sdn.calP_plot(); // show plot of power spectrum of zeta
 
   // --------- stop stopwatch ----------------
@@ -80,26 +97,37 @@ int main(int argc, char** argv)
 }
 
 
-// ---------- Lagrangian params. and diff. coeff.  X[0]=phi ----------
+// ---------- Lagrangian params. and diff. coeff.  X[0]=phi, X[1]=psi ----------
 
 double StocDeltaN::V(vector<double> &X)
 {
-  return LAMBDA*LAMBDA*LAMBDA*LAMBDA*(1-X[0]*X[0]/MU/MU);
+  return LAMBDA4*((1-X[1]*X[1]/MM/MM)*(1-X[1]*X[1]/MM/MM)
+		  + 2*X[0]*X[0]*X[1]*X[1]/PHIC/PHIC/MM/MM + (X[0]-PHIC)/MU1
+		  - (X[0]-PHIC)*(X[0]-PHIC)/MU2/MU2);
 }
 
 double StocDeltaN::VI(vector<double> &X, int I) // \partial_I V
 {
-  return -2*X[0]*LAMBDA*LAMBDA*LAMBDA*LAMBDA/MU/MU;
+  if (I == 0) {
+    return LAMBDA4*(1./MU1 - 2*(X[0]-PHIC)/MU2/MU2 + 4*X[0]*X[1]*X[1]/PHIC/PHIC/MM/MM);
+  } else {
+    return LAMBDA4*(4*X[0]*X[0]*X[1]/PHIC/PHIC/MM/MM
+		    - 4*X[1]*(1-X[1]*X[1]/MM/MM)/MM/MM);
+  }
 }
 
 double StocDeltaN::metric(vector<double> &X, int I, int J) // G_IJ
 {
-  return 1;
+  if (I == J) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 double StocDeltaN::inversemetric(vector<double> &X, int I, int J) // G^IJ
 {
-  return 1;
+  return metric(X,I,J);
 }
 
 double StocDeltaN::affine(vector<double> &X, int I, int J, int K) // \Gamma^I_JK
